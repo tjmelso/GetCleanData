9020878f5391041ffb55963fd8d009e96f606574
tjmelso <tymelso@gmail.com>
2015-10-19 20:55:35 -0500

    completed R script

---
+++ README.md
@@ -0,0 +1,8 @@
+==================================================================
+Getting and Cleaning Data Course Project
+Tyler Melso
+==================================================================
+
+There is only one script I wrote for this project and it is pretty straight forward. 
+
+
---
+++ run_analysis.R
@@ -0,0 +1,37 @@
+#### Getting and Cleaning Data Course Project
+library(dplyr)
+#Set working directory and read data sets into R
+setwd("C:/Users/tymel_000/Repos/GetCleanData/UCI HAR Dataset")
+xtest <- read.table("test/X_test.txt")
+ytest <- read.table("test/y_test.txt", col.names="Activity")
+subtest <- read.table("test/subject_test.txt", col.names = "ID")
+xtrain <- read.table("train/X_train.txt")
+ytrain <- read.table("train/y_train.txt", col.names = "Activity")
+subtrain <- read.table("train/subject_train.txt", col.names = "ID")
+# First merge x, y, and sub tables together
+mergedtest <- cbind(subtest, ytest, xtest)
+mergedtrain <- cbind(subtrain, ytrain, xtrain)
+# Now merge test and train datasets together
+both <- rbind(mergedtest, mergedtrain)
+
+activity <- read.table("activity_labels.txt", stringsAsFactors=FALSE)
+for(i in seq_along(activity$V2)){ #Put the activity labels into my dataset
+    both$Activity <- gsub(i, activity[i,2], both$Activity)
+}
+
+features <- read.table("features.txt", stringsAsFactors=FALSE)
+for(i in nrow(features):1){ #Only saving the mean and std columns
+    if(grepl("mean", features[i,2])){
+        colnames(both)[i+2] <- features$V2[i]
+    }
+    else if(grepl("std", features[i,2])){
+        colnames(both)[i+2] <- features$V2[i]
+    }
+    else{
+        both[,i] <- NULL
+    }
+}
+# And finally average out all duplicated activities by same ID
+final <- aggregate(both[, -c(1:2)], by = list(both$ID, both$Activity), mean)
+final <- rename(final, ID = Group.1, Activity = Group.2)
+write.csv(final, "../tidydata.csv", row.names=FALSE)
